af2a05b19206eccf935c2f439123bf68785594f7
MicaelaDias <132285031+MicaelaDias@users.noreply.github.com>
2023-06-04 19:32:55 -0300

    Add files via upload

---
+++ Ejercicio5/Interfaz.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+class  Interfaz(Interface):
+    def insertarElemento(elemento,posicion):
+        pass
+    def agregarElemento(elemento):
+        pass
+    def mostrarElemento(pos):
+        pass
